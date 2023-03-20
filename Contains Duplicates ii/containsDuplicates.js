2b10b950d88c4823a2e6ed65db62325aad3289ec
Prajwal77777 <96177425+Prajwal77777@users.noreply.github.com>
2023-03-20 22:30:09 +0545

    Create containsDuplicates.js

---
+++ Contains Duplicates ii/containsDuplicates.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    let map =[];
+    
+    for(let i =0; i < nums.length; i++){
+        if(i-map[nums[i]] <= k){
+            return true;
+        }else{
+            map[nums[i]] = [i];
+        }
+        
+    }
+    return false;
+};
