156e9187643463945981b48d2691604dc6d9ac3e
Prajwal77777 <96177425+Prajwal77777@users.noreply.github.com>
2023-03-27 21:29:46 +0545

    Create toLowerCase.java

---
+++ LowerCase/toLowerCase.java
@@ -0,0 +1,10 @@
+Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+  
+  
+  
+  class Solution {
+    public String toLowerCase(String s) {
+        String LowerCase = s.toLowerCase();
+        return LowerCase;
+    }
+}
