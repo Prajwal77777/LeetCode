b15c74573c5fcb49c4cb162cdd79d7595c5caad6
Prajwal77777 <96177425+Prajwal77777@users.noreply.github.com>
2023-03-29 23:17:15 +0545

    Create FlippingAnImage.java

---
+++ Flipping an Image/FlippingAnImage.java
@@ -0,0 +1,41 @@
+Given an n x n binary matrix image, flip the image horizontally, then invert it, and return the resulting image.
+
+To flip an image horizontally means that each row of the image is reversed.
+
+For example, flipping [1,1,0] horizontally results in [0,1,1].
+To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0.
+
+For example, inverting [0,1,1] results in [1,0,0].
+  
+  
+  //CODE
+  class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        int n = image.length;
+        
+        // Flip the image horizontally
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+            while (left < right) {
+                int temp = image[i][left];
+                image[i][left] = image[i][right];
+                image[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+        
+        // Invert the image
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (image[i][j] == 0) {
+                    image[i][j] = 1;
+                } else {
+                    image[i][j] = 0;
+                }
+            }
+        }
+        
+        return image;
+    }
+}
