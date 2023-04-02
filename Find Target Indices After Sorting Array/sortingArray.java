e61ad0abb0a8a094dd051ce3a15961c465644cd5
Prajwal77777 <96177425+Prajwal77777@users.noreply.github.com>
2023-04-02 21:14:34 +0545

    Create sortingArray.java

---
+++ Find Target Indices After Sorting Array/sortingArray.java
@@ -0,0 +1,50 @@
+You are given a 0-indexed integer array nums and a target element target.
+
+A target index is an index i such that nums[i] == target.
+
+Return a list of the target indices of nums after sorting nums in non-decreasing order. If there are no target indices, return an empty list. The returned list must be sorted in increasing order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,5,2,3], target = 2
+Output: [1,2]
+Explanation: After sorting, nums is [1,2,2,3,5].
+The indices where nums[i] == 2 are 1 and 2.
+Example 2:
+
+Input: nums = [1,2,5,2,3], target = 3
+Output: [3]
+Explanation: After sorting, nums is [1,2,2,3,5].
+The index where nums[i] == 3 is 3.
+Example 3:
+
+Input: nums = [1,2,5,2,3], target = 5
+Output: [4]
+Explanation: After sorting, nums is [1,2,2,3,5].
+The index where nums[i] == 5 is 4.
+  
+  
+  
+  //SOLUTION
+  
+  
+  class Solution {
+    public List<Integer> targetIndices(int[] nums, int target) {
+        int count = 0; int lessThen = 0;
+        for(int num : nums){
+            if(num == target){
+                count++;
+            }
+            if(num < target){
+                lessThen++;
+            }
+        }
+        List<Integer> result = new ArrayList<> ();
+        for(int i = 0; i < count;i++){
+            result.add(lessThen++);
+        }
+        return result;
+    }
+}
